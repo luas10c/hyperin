563d5c7221a12bb90c6f2cf9cf0bb4f97d640c76
Luciano Alves <luas10c@gmail.com>
2026-04-18 08:29:11 -0300

    test: fix typo

--- jest.config.js
+++ jest.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
         '^.+\\.(t|j)sx?$': '@swc/jest'
       },
       moduleNameMapper: {
-        '#^(.+)$': '<rootDir>/src/$1'
+        '^#/(.+)$': '<rootDir>/src/$1'
       }
     },
     {
@@ -22,7 +22,7 @@ export default defineConfig({
         '^.+\\.(t|j)sx?$': '@swc/jest'
       },
       moduleNameMapper: {
-        '#^(.+)$': '<rootDir>/src/$1'
+        '^#/(.+)$': '<rootDir>/src/$1'
       }
     }
   ]
